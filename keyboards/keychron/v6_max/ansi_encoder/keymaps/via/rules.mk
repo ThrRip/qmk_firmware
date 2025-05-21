1959b80b0bd3db8a67285c2634963f56e0927b91
ThrRip <coding@thrrip.space>
2025-05-22 01:13:08 +0800

    Use `KEYBOARD_SHARED_EP` to free up an endpoint instead

--- keyboards/keychron/v6_max/ansi_encoder/keymaps/via/rules.mk
+++ keyboards/keychron/v6_max/ansi_encoder/keymaps/via/rules.mk
@@ -1,8 +1,11 @@
 VIA_ENABLE = yes
 
 # ThrRip start
-EXTRAKEY_ENABLE = no
-MOUSEKEY_ENABLE = no
-NKRO_ENABLE     = no
-MIDI_ENABLE     = yes
+# If you experience issues with the keyboard in BIOS, try to disable
+# "KEYBOARD_SHARED_EP" and uncomment the following 3 lines.
+# EXTRAKEY_ENABLE  = no
+# MOUSEKEY_ENABLE  = no
+# NKRO_ENABLE      = no
+KEYBOARD_SHARED_EP = yes
+MIDI_ENABLE        = yes
 # ThrRip end
